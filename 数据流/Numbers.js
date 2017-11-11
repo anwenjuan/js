c4af59d4d493bf0196b59a799a1a311f66433b1b
Clarence <azzbcc@sina.com>
2017-11-12 02:49:18 +0800

    数字更新计时器分离

--- 数据流/Numbers.js
+++ 数据流/Numbers.js
@@ -1,72 +1,53 @@
-/**
- * Created by anan on 2017/11/11.
- */
-function Numbers() {
+function DataStream(ulCount, liCount) {
+    ulCount = ulCount || parseInt(getRandom(10, 30));
+    liCount = liCount || parseInt(getRandom(10, 12));
     this.init = function (box) {
-        for (var i = 0; i < parseInt(Math.random() * 50); i++) {
+        for (var i = 0; i < ulCount; i++) {
             var ul = document.createElement('ul');
-            var wordSize = getRandomIntInclusive(6, 14) * 2;
-            var liHeight = wordSize;
-            for (var j = 0; j < 8; j++) {
+            var wordSize = parseInt(getRandom(12, 28));
+            for (var j = 0; j < liCount; j++) {
                 var li = document.createElement('li');
                 li.style.fontSize = wordSize + 'px';
                 li.innerHTML = parseInt(Math.random() * 10);
-                li.style.lineHeight = liHeight + 'px';
-                li.style.paddingTop = '2px';
+                li.style.lineHeight = wordSize + 'px';
+                // li.style.paddingTop = '1px';
                 ul.appendChild(li);
             }
             ul.style.width = wordSize + 'px';
+
             var top = Math.random() * (box.clientHeight);
+            ul.style.top = top + 'px';
 
             var left = Math.random() * (box.clientWidth);
             ul.style.left = left + 'px';
-            ul.style.top = top + 'px';
+
             box.appendChild(ul);
+
             animate(ul, box.clientHeight);
         }
-    }
+    };
 }
 
-
-function animate(ele, target) {
-    clearInterval(ele.timer);
-    ele.timer = setInterval(function () {
-        var liArr = ele.children;
-        for (var i = 0; i < liArr.length; i++) {
-            liArr[i].innerHTML = parseInt(Math.random() * 10);
-        }
-
-
-        var step = Math.random();
-        ele.style.top = ele.offsetTop + step + "px";
-        console.log(111)
-        if (Math.abs(target - ele.offsetTop) <= Math.abs(step)) {
-            ele.style.top = target + "px";
-            clearInterval(ele.timer);
+function animate(ul, target) {
+    clearInterval(ul.timer);
+    clearInterval(ul.childTimer);
+    var step = getRandom(0.5, 1);
+    ul.timer = setInterval(function () {
+        ul.style.top = ul.offsetTop + step + "px";
+        if (Math.abs(target - ul.offsetTop) <= Math.abs(step)) {
+            ul.style.top = target + "px";
+            clearInterval(ul.timer);
+            clearInterval(ul.childTimer);
         }
     }, 1);
+    ul.childTimer = setInterval(function () {
+        var liArray = ul.children;
+        for (var i = 0;i < liArray.length;i++) {
+            liArray[i].innerHTML = parseInt(Math.random() * 10);
+        }
+    }, 200);
 }
 
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
 }
-
-// function liNumber(li) {
-//     clearInterval(li.timer);
-//
-//
-//     li.timer = setInterval(function () {
-//         li.innerHTML = parseInt(Math.random() * 10);
-//
-//         if(li.parentNode.style.top >= li.parentNode.parentNode.clientHeight) {
-//             clearInterval(li.timer);
-//         }
-//         // console.log(li.parentNode);
-//         // clearInterval(timer);
-//         // console.log(li.parentNode.style.top);
-//         console.log(111)
-//     },1500)
-// }
-
