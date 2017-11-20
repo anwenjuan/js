df8d11ba7fd7ac221df28e33c9243ed404ff2960
Clarence <azzbcc@sina.com>
2017-11-21 00:15:35 +0800

    通过href 传参 简单实现跳转不同图片

--- 艺网/js/link.js
+++ 艺网/js/link.js
@@ -1,18 +1,11 @@
-
 window.onload = function () {
-        var imgArr = document.getElementsByTagName('img');
-        for (var i = 0; i < imgArr.length; i++) {
-            imgArr[i].onclick = function () {
-
-                return function () {
-                    var imgAddress = this.src;
-                    // window.imgAddress = imgAddress;
-                    location.href = 'magnify.html';
-
-                }
-
-            }();
+    var imgArr = document.getElementsByTagName('img');
+    for (var i = 0; i < imgArr.length; i++) {
+        imgArr[i].onclick = function () {
+            //FIXME ? 为什么原先的写法是retrun function
+            var imgAddress = encodeURIComponent(this.attributes.src.nodeValue);
+            console.log(imgAddress);
+            location.href = 'magnify.html?src=' + imgAddress;
         }
-
-    window.imgAddress = imgAddress;
     }
+};
